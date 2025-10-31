7799110b9d1abadb6fb02c96eb1e193fa4658b81
yonas-baysasaw <yonas.baysasaw.kibret@gmail.com>
2025-10-31 11:38:26 +0300

    Expore more core components

--- prodev-mobile-app-0x03/app/_layout.tsx
+++ prodev-mobile-app-0x03/app/_layout.tsx
@@ -1,11 +1,7 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
-  );
+  const screenOptions = { headerShown: false };
+
+  return <Stack screenOptions={screenOptions} />;
 }
